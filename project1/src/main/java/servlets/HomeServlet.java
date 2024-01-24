d0db8e1c715714085d007c9a606469cc93d30cd4
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-01-24 11:23:04 +0100

    push database connection

--- project1/src/main/java/servlets/HomeServlet.java
+++ project1/src/main/java/servlets/HomeServlet.java
@@ -7,13 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 @WebServlet(urlPatterns = "/home")
 public class HomeServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
 
         PrintWriter out = resp.getWriter();
         out.println("<html>");
@@ -24,5 +27,25 @@ public class HomeServlet extends HttpServlet {
         out.println("</html>");
         System.out.println("GET Request");
         System.out.println(req.getParameter("name"));
+        connect();
+    }
+
+    public void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+//PORT and DbName should be changed
+
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/Grit", "root", "");
+            Statement stmt = con.createStatement();
+//TABLENAME should be changed
+
+            ResultSet rs = stmt.executeQuery("SELECT * FROM students");
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + " " + rs.getString(2));
+            }
+            con.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
